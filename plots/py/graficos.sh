9ef568051a386b7cb3532eec23a38874c482ab3e
cleyber-bezerra <cleyberb@gmail.com>
2024-11-04 11:31:34 -0300

    remove pasta csv

--- plots/py/graficos.sh
+++ plots/py/graficos.sh
@@ -5,12 +5,14 @@
 python barra_delay.py
 python barra_energy.py
 python barra_loss.py
+python barra_jitter.py
 python barra_throughput.py
 
 python line_delay.py
 python line_energy.py
 python line_loss.py
 python line_throughput.py
+python line_jitter.py
 python line_all.py
 
 exit 0
